768d61b95ecdb76c139dae9dfa89cd3486a5d2df
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-04-02 00:09:25 +0100

    Create Worlds.java

---
+++ Java/src/main/java/xyz/pikzstudio/Worlds.java
@@ -0,0 +1,56 @@
+package xyz.pikzstudio;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class Worlds implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            ChatColor error = ChatColor.DARK_RED;
+            sender.sendMessage(error + "Only players can use this command!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        createSmallWorld(player);
+        return true;
+    }
+
+    private void createSmallWorld(Player player) {
+        String worldName = player.getName() + "_world";
+        WorldCreator worldCreator = new WorldCreator(worldName);
+        World smallWorld = Bukkit.createWorld(worldCreator);
+
+         double borderSize = 15;
+         Bukkit.World.getWorldBorder().setSize(borderSize);
+
+        ChatColor gray = ChatColor.DARK_GRAY;
+        ChatColor green = ChatColor.GREEN;
+        ChatColor white = ChatColor.WHITE;
+
+        player.sendMessage(white + "");
+        player.sendMessage(green + "" + ChatColor.BOLD + "WORLD CREATED");
+        player.sendMessage(white + "");
+        player.sendMessage(gray + "• " + white + "Manage World" + green + " [/world manage]");
+        player.sendMessage(gray + "• " + white + "Visit Worlds" + green + " [/world visit]");
+        player.sendMessage(gray + "• " + white + "Teleport to World" + green + " [/world teleport]");
+        player.sendMessage(white + "");
+        player.sendMessage("You have been teleported to your world automatically. Return to spawn using" + green + " /spawn");
+        player.sendMessage(white + "");
+        String worldName = player.getName() + "_world";
+        World world = Bukkit.getWorld(worldName);
+
+        player.teleport(world.getSpawnLocation());
+
+
+    }
+}
