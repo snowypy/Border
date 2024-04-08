17c4a730f31135132ec0349742a2a7ac0a62af99
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-04-08 19:24:47 +0100

    Update MissionListener.java

--- Java/src/main/java/xyz/pikzstudio/MissionCore/MissionListener.java
+++ Java/src/main/java/xyz/pikzstudio/MissionCore/MissionListener.java
@@ -1,10 +1,15 @@
 package xyz.pikzstudio.MissionCore;
 
+import net.kyori.adventure.platform.facet.Facet;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class MissionListener implements Listener {
 
@@ -22,8 +27,28 @@ public class MissionListener implements Listener {
         }
     }
 
+
+
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         missionsData.handleBlockBreak(event.getPlayer(), event.getBlock().getType());
     }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getClickedInventory() != null && ChatColor.stripColor(event.getView().getTitle()).equals("Missions")) {
+            event.setCancelled(true);
+        }
+
+        Player player = (Player) event.getWhoClicked();
+        if (event.getView().getTitle().equals("Missions Manager")) {
+            event.setCancelled(true);
+            ItemStack clickedItem = event.getCurrentItem();
+            if (clickedItem != null && clickedItem.getType() == Material.BARRIER) {
+                player.performCommand("missions start");
+                player.performCommand("missions gui");
+            }
+        }
+    }
+
 }
