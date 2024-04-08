e0ffd564b564140d76b7ed23076bd64ed164aee0
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-04-08 19:24:51 +0100

    Update SessionListener.java

--- Java/src/main/java/xyz/pikzstudio/Utils/SessionListener.java
+++ Java/src/main/java/xyz/pikzstudio/Utils/SessionListener.java
@@ -1,31 +1,38 @@
 package xyz.pikzstudio.Utils;
 
-import org.bukkit.OfflinePlayer;
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import xyz.pikzstudio.Border;
+
 public class SessionListener implements Listener {
+
+    private final Border plugin;
+
+    public SessionListener(Border plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
+        String joinMessage = plugin.getPluginConfig().getString("server.game.messages.joinMessage", "Debug: §7[§a+&7] §a{name} §7has joined");
 
-        String playername = event.getPlayer().getName();
+        joinMessage = joinMessage.replace("<name>", playerName);
+        joinMessage = joinMessage.replace("&", "§");
 
-        // Join Message
-        event.setJoinMessage("§7[§a+§7] §a" + playername + " §7has joined");
-
+        event.setJoinMessage(joinMessage);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
+        String playerName = event.getPlayer().getName();
+        String quitMessage = plugin.getPluginConfig().getString("server.game.messages.quitMessage", "Debug: §7[§c-§7] §c{name} §7has disconnected");
 
-        String playername = event.getPlayer().getName();
+        quitMessage = quitMessage.replace("<name>", playerName);
+        quitMessage = quitMessage.replace("&", "§");
 
-        // Quit Message
-        event.setQuitMessage("§7[§c-§7] §c" + playername + " §7has disconnected");
-
+        event.setQuitMessage(quitMessage);
     }
-
 }
